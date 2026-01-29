f253bb873aad346387bff52e67a6748976e8d28c
thedanielguodev <thedanielguo@gmail.com>
2026-01-28 18:21:27 -0800

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,8 +1,21 @@
-weekly_minutes = 0
+weekly_practice_score = 0
+weekly_match_score = 0
 
-def add_practice_minutes(minutes):
-    global weekly_minutes
-    weekly_minutes += minutes
+def add_practice(minutes, intensity):
+    global weekly_practice_score
+    weekly_practice_score += minutes * intensity
 
-def get_weekly_minutes():
-    return weekly_minutes
+def add_match(opponent_level, result):
+    global weekly_match_score
+
+    if result == "win":
+        weekly_match_score += opponent_level * 1.5
+    else:
+        weekly_match_score += opponent_level * 0.7
+
+def get_effectiveness():
+    return round(
+        (weekly_practice_score * 0.6) +
+        (weekly_match_score * 0.4),
+        1
+    )
